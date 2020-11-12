df672329309409b2b2cacd7e4ea1efdeade0b280
CSRGH27 <universaltaskmail@gmail.com>
2020-11-12 17:42:15 +0100

    domain name

--- config/deploy_prod.php
+++ config/deploy_prod.php
@@ -10,7 +10,7 @@ return new class extends DefaultDeployer
     {
         return $this->getConfigBuilder()
             ->server('debian@51.178.50.201')
-            ->deployDir('/var/www/life_admin')
+            ->deployDir('/var/www/systemeX')
             ->repositoryUrl('git@github.com:CSRGH27/life_admin.git')
             ->repositoryBranch('develop')
             ->symfonyEnvironment('prod');
